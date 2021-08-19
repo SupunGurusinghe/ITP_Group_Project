8b59f3dc8accef6b37bb9edc95afdb58ede7f24e
Nethmi Amanda <86509762+nethmiamanda99@users.noreply.github.com>
2021-08-19 21:49:58 +0530

    Create Employee Management

--- emp_roles/apps.py
+++ emp_roles/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmpRoleConfig(AppConfig):
+class EmpRolesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'emp_role'
+    name = 'emp_roles'
